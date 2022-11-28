1ad3ca24b0adfce751fa78fdf3d4043f5002fc11
Cba_Vin <vin@cybridge.jp>
2022-11-28 22:42:44 +0700

    get data in database

--- src/server.js
+++ src/server.js
@@ -1,23 +1,24 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import viewEngine from './config/viewEngine';
-import initWebRoutes from './route/web';
-require('dotenv').config();
+import express from "express";
+import bodyParser from "body-parser";
+import viewEngine from "./config/viewEngine";
+import initWebRoutes from "./route/web";
+import connectDB from "./config/connectDB";
+
+require("dotenv").config();
 
 const app = express();
 
 //config app
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 viewEngine(app);
-
 initWebRoutes(app);
+
+connectDB();
 
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
   console.log(`server is running on the ${port}`);
 });
-
-
